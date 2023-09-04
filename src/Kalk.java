6e0af8d0b9ffbd3a47afa6ef56443d396db62fe7
Theodore Hammar <theodore.hammar@elev.ga.ntig.se>
2023-09-04 08:57:23 +0200

    Nummer 1 Och 2

--- src/Kalk.java
+++ src/Kalk.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Kalk {
     public static void main(String[] args) throws Exception {
+
+        Scanner tb = new Scanner(System.in);
+
+        System.out.println("Ange nummer 1");
+        int nummer1 = tb.nextInt();
+        System.out.println("Ange nummer 2");
+        int nummer2 = tb.nextInt();
+        System.out.println("1=+, 2=-, 3=*, 4=/");
+        int metod = tb.nextInt();
+
         
+
     }
 }
+
+
